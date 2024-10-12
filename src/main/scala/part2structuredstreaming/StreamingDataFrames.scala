8a14be34deb20e59a699bb7e45f9c0bad2cee95f
PRITISH009 <pritishgupta009@gmail.com>
2024-10-12 16:59:18 +0530

    Removing Everything to write on my own

--- src/main/scala/part2structuredstreaming/StreamingDataFrames.scala
+++ src/main/scala/part2structuredstreaming/StreamingDataFrames.scala
@@ -5,53 +5,6 @@ import org.apache.spark.sql.functions._
 import org.apache.spark.sql.streaming.StreamingQuery
 import org.apache.spark.sql.{DataFrame, SparkSession}
 
-object StreamingDataFrames {
-
-  // Step 1: Create a spark session -
-  val spark = SparkSession.builder()
-    .appName("Our First Streams")
-    .master("local[*]")
-    .getOrCreate()
-
-  def readFromSocket() = {
-    // Dataframes can also be streaming dataframes
-    val lines: DataFrame = spark.readStream.format("socket")
-      .option("host", "localhost")
-      .option("port", "12345")
-      .load()
-
-    val shortLines = lines.where(length(col("value")) <= 10)
-
-    // How to tell if a data is static of streaming
-    println(shortLines.isStreaming)
-
-    // Defining A sink and starting the sink
-    val query: StreamingQuery = shortLines.writeStream
-      .format("console")
-      .outputMode("append")
-      .start()
-
-    query.awaitTermination()
-  }
-
-  def readFromFile() = {
-    val stocksDF = spark.readStream
-      .format("csv")
-      .option("header", "false")
-      .option("dateFormat", "MMM d yyyy")
-      .schema(stocksSchema)
-      .load("src/main/resources/stocks")
-
-
-    stocksDF.writeStream
-      .format("console")
-      .outputMode("append")
-      .start()
-      .awaitTermination()
-  }
-
-  def main(args: Array[String]): Unit = {
-    readFromFile()
-  }
-
+object StreamingDataFrames extends App {
+    
 }
